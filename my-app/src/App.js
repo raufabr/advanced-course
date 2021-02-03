363c538b3612eb58cfe2420f6dc2fe8c263cb388
Abrar Rauf <abrarrauf@192.168.1.104>
2021-02-03 17:53:12 +0000

    hard coded site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "../src/components/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faTemperatureHigh,
+	faTemperatureLow,
+	faCity,
+	faClock,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+
+			<div className="search-bar">Search bar</div>
+
+			<div className="container">
+				<div className="box location">
+					<FontAwesomeIcon icon={faCity} size="3x" />
+					<div className="location-text">London</div>
+				</div>
+				<div className="box location">
+					{" "}
+					<FontAwesomeIcon icon={faTemperatureHigh} size="3x" />
+					High Temperature
+				</div>
+				<div className="box location">
+					{" "}
+					<FontAwesomeIcon icon={faTemperatureLow} size="3x" />
+					Low Temperature
+				</div>
+				<div className="box location">Humidity</div>
+			</div>
+			<div className="daily-temp">
+				<div className="daily-temp-row">
+					<div>
+						{" "}
+						<FontAwesomeIcon icon={faClock} size="xl" /> 6AM
+					</div>
+					<div>Temperature</div>
+					<div>Humidity</div>
+				</div>
+				<div className="daily-temp-row box">
+					<div>
+						<FontAwesomeIcon icon={faClock} size="xl" /> 12AM
+					</div>
+					<div>Temperature: 24C</div>
+					<div>Humidity</div>
+				</div>
+				<div className="daily-temp-row">
+					<div>
+						{" "}
+						<FontAwesomeIcon icon={faClock} size="xl" /> 18PM
+					</div>
+					<div>Temperature</div>
+					<div>Humidity</div>
+				</div>
+				<div className="daily-temp-row">
+					<div>
+						<FontAwesomeIcon icon={faClock} size="xl" /> 00AM
+					</div>
+					<div>Temperature</div>
+					<div>Humidity</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
+
+export default function Navbar() {
+	return (
+		<nav className="navbar is-dark">
+			<div className="navbar-menu">
+				<div class="navbar-brand">
+					<FontAwesomeIcon icon={faCloudSunRain} /> Weather app
+				</div>
+			</div>
+		</nav>
+	);
+}
