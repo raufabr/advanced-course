372be332eb0cfdd0e92eb77913c78273d03a9b57
Abrar Rauf <abrarrauf@Abrars-MacBook-Pro.local>
2021-04-13 14:55:06 +0100

    Adding files from lesson3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,68 +1,80 @@
 import "./App.css";
 import Navbar from "../src/components/Navbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faTemperatureHigh,
-	faTemperatureLow,
-	faCity,
-	faClock,
-} from "@fortawesome/free-solid-svg-icons";
-
+import React, { useState } from "react";
+import { getData } from "./api/location";
+import Day from "./components/Day";
 function App() {
+	const cities = ["Belfast", "Dublin", "Edinburgh", "London"];
+	const [selectCity, setSelectCity] = useState("Belfast");
+	const [data, setData] = useState([]);
+	const [startDate, setStartDate] = useState("2021-01-01");
+	const [endDate, setEndDate] = useState("2021-01-07");
+
+	const handleChange = (value) => {
+		setSelectCity(value);
+	};
+	const handleSubmit = async () => {
+		const response = await getData(startDate, endDate, selectCity);
+		setData(response);
+	};
 	return (
 		<div className="App">
 			<Navbar />
-
-			<div className="search-bar">Search bar</div>
-
-			<div className="container">
-				<div className="box location">
-					<FontAwesomeIcon icon={faCity} size="3x" />
-					<div className="location-text">London</div>
+			<div className="selectBoxContainer">
+				<div
+					className="select is-normal is-rounded is-primary"
+					id="selectBox">
+					<select
+						onChange={(e) => handleChange(e.target.value)}
+						value={selectCity}>
+						{cities.map((city, index) => {
+							return <option key={index}>{city}</option>;
+						})}
+					</select>
 				</div>
-				<div className="box location">
-					{" "}
-					<FontAwesomeIcon icon={faTemperatureHigh} size="3x" />
-					High Temperature
+				<div className="date-fields">
+					<input
+						className="is-primary is-normal is-rounded"
+						type="date"
+						id="start"
+						name="start"
+						value={startDate}
+						onChange={(e) => setStartDate(e.target.value)}
+						min="2021-01-01"
+						max="2021-01-07"
+					/>
 				</div>
-				<div className="box location">
-					{" "}
-					<FontAwesomeIcon icon={faTemperatureLow} size="3x" />
-					Low Temperature
+				<div className="date-fields">
+					<input
+						type="date"
+						id="start"
+						name="end"
+						value={endDate}
+						onChange={(e) => setEndDate(e.target.value)}
+						min="2021-01-01"
+						max="2021-01-07"
+					/>
 				</div>
-				<div className="box location">Humidity</div>
+				<div>
+					<button
+						className="button is-rounded is-primary btnContainer"
+						onClick={handleSubmit}>
+						Submit
+					</button>
+				</div>
 			</div>
-			<div className="daily-temp">
-				<div className="daily-temp-row">
-					<div>
-						{" "}
-						<FontAwesomeIcon icon={faClock} size="xl" /> 6AM
-					</div>
-					<div>Temperature</div>
-					<div>Humidity</div>
-				</div>
-				<div className="daily-temp-row box">
-					<div>
-						<FontAwesomeIcon icon={faClock} size="xl" /> 12AM
-					</div>
-					<div>Temperature: 24C</div>
-					<div>Humidity</div>
-				</div>
-				<div className="daily-temp-row">
-					<div>
-						{" "}
-						<FontAwesomeIcon icon={faClock} size="xl" /> 18PM
-					</div>
-					<div>Temperature</div>
-					<div>Humidity</div>
-				</div>
-				<div className="daily-temp-row">
-					<div>
-						<FontAwesomeIcon icon={faClock} size="xl" /> 00AM
-					</div>
-					<div>Temperature</div>
-					<div>Humidity</div>
-				</div>
+			<div>
+				{data?.length > 0 && typeof data !== "string" ? (
+					data.map((date) => {
+						return <Day date={date} />;
+					})
+				) : (
+					<>
+						{typeof data === "string"
+							? "Data not available in the backend"
+							: "Please enter a city"}
+					</>
+				)}
 			</div>
 		</div>
 	);
